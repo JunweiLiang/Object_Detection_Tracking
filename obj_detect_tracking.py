c47733781a18e073e72570f73f1ef9fada1711ec
18511863357 <chunwaileong@ryoushunweideMacBook-Air.local>
2019-06-05 08:06:00 -0700

    removed unused cpu_nms and soft_nms

--- obj_detect_tracking.py
+++ obj_detect_tracking.py
@@ -166,6 +166,7 @@ def get_args():
 		args.use_se = True
 
 	# ---------------more defautls
+	args.is_pack_model = False
 	args.diva_class3 = True
 	args.diva_class = False
 	args.diva_class2 = False
